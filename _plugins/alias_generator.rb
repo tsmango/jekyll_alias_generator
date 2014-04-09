7c80d261a035c70bcab5c2b8dd967ac5c6292a2e
Tom Mango <tom@cloudwarmerinc.com>
2014-04-09 07:32:46 -0400

    Fixed a typo and outdated link in the documentation.

--- _plugins/alias_generator.rb
+++ _plugins/alias_generator.rb
@@ -24,8 +24,8 @@
 # Author: Thomas Mango
 # Site: http://thomasmango.com
 # Plugin Source: http://github.com/tsmango/jekyll_alias_generator
-# Site Source: http://github.com/tsmango/thomasmango.com
-# PLugin License: MIT
+# Site Source: http://github.com/tsmango/tsmango.github.com
+# Plugin License: MIT
 
 module Jekyll
 
