abbe0e672edb3cba04ea6161bc4ea985df3949a1
Joel Meyer-Hamme <joelmeyerhamme@me.com>
2014-07-08 16:16:09 +0200

    Optional page root slash in alias path

--- _plugins/alias_generator.rb
+++ _plugins/alias_generator.rb
@@ -70,8 +70,9 @@ module Jekyll
           file.write(alias_template(destination_path))
         end
 
-        (alias_index_path.split('/').size).times do |sections|
-          @site.static_files << Jekyll::AliasFile.new(@site, @site.dest, alias_index_path.split('/')[1, sections + 1].join('/'), '')
+        sections = alias_index_path.split('/').reject(&:empty?)
+          sections.size.times do |n|
+          @site.static_files << Jekyll::AliasFile.new(@site, @site.dest, sections[0, n+1].join('/'), '')
         end
       end
     end
